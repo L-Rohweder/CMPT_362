ae533b2bc81503d06fed25f7b19db7d363d93cd1
Jonathan Gibbons <jcg12@sfu.ca>
2024-11-07 21:36:38 -0800

    Refactored taking pictures and added to post fragment

--- frontend/app/src/main/java/com/example/beacon/fragments/PostCreateFragment.kt
+++ frontend/app/src/main/java/com/example/beacon/fragments/PostCreateFragment.kt
@@ -1,5 +1,8 @@
 package com.example.beacon.fragments
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -7,8 +10,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.android.volley.Request
@@ -18,6 +25,7 @@ import com.example.beacon.R
 import com.example.beacon.databinding.FragmentPostCreateBinding
 import com.example.beacon.models.BeaconPost
 import com.example.beacon.utils.Constants.BACKEND_IP
+import com.example.beacon.utils.ImageHandler
 import com.example.beacon.view_models.UserViewModel
 import kotlinx.serialization.json.Json
 import org.json.JSONObject
@@ -25,6 +33,9 @@ import org.json.JSONObject
 class PostCreateFragment : Fragment() {
 
     private var _binding: FragmentPostCreateBinding? = null
+    private lateinit var imageHandler: ImageHandler
+    private lateinit var  image: ImageView
+    private lateinit var cameraResult: ActivityResultLauncher<Intent>
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -35,12 +46,29 @@ class PostCreateFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentPostCreateBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
+        _binding = FragmentPostCreateBinding.inflate(inflater, container, false)
+        imageHandler = ImageHandler(requireContext())
+        val root: View = binding.root
+        val addImageButton = root.findViewById<Button>(R.id.postAddImage)
         val publishPostButton = root.findViewById<Button>(R.id.publishPostButton)
+        image = root.findViewById<ImageView>(R.id.postImageView)
+
+        cameraResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+                result: ActivityResult ->
+            if(result.resultCode == Activity.RESULT_OK){
+                val bitmap: Bitmap = imageHandler.getBitmap(requireContext(), imageHandler.getTempImgUri())
+                image.setImageBitmap(bitmap)
+            }
+
+        }
+
         publishPostButton.setOnClickListener {
             publishPost()
+        }
+        addImageButton.setOnClickListener(){
+            imageHandler.checkCameraPerms(requireActivity())
+            imageHandler.showDialog(requireActivity(), cameraResult)
         }
 
         return root
@@ -50,6 +78,7 @@ class PostCreateFragment : Fragment() {
         val username = _binding?.root?.findViewById<EditText>(R.id.usernameEditText)?.text
         val content = _binding?.root?.findViewById<EditText>(R.id.contentEditText)?.text
         val userViewModel = ViewModelProvider(requireActivity())[UserViewModel::class.java]
+
         userViewModel.requestedLocation.value = true
         userViewModel.location.observe(requireActivity()) {
             if (userViewModel.requestedLocation.value == true) {
