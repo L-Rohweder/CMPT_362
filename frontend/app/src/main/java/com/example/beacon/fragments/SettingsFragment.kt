b82257268d484b7ccc7a8971274321d937caad7d
Jonathan Gibbons <jcg12@sfu.ca>
2024-11-07 17:31:18 -0800

    Added profile button to settings fragment

--- frontend/app/src/main/java/com/example/beacon/fragments/SettingsFragment.kt
+++ frontend/app/src/main/java/com/example/beacon/fragments/SettingsFragment.kt
@@ -1,10 +1,15 @@
 package com.example.beacon.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.example.beacon.R
+import com.example.beacon.activities.ProfileActivity
 import com.example.beacon.databinding.FragmentSettingsBinding
 
 class SettingsFragment : Fragment() {
@@ -14,6 +19,7 @@ class SettingsFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private lateinit var profileButton: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,6 +28,11 @@ class SettingsFragment : Fragment() {
     ): View {
         _binding = FragmentSettingsBinding.inflate(inflater, container, false)
         val root: View = binding.root
+        profileButton = root.findViewById<TextView>(R.id.openProfileText)
+        profileButton.setOnClickListener(){
+            val intent = Intent(activity, ProfileActivity::class.java)
+            startActivity(intent)
+        }
         return root
     }
 
